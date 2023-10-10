b14ade3525712052efebdef2c43e80ae0aba0acc
Jason Pickering <jason.p.pickering@gmail.com>
2023-10-10 09:41:54 +0200

    Merge with DHIS2 core (#16)
    * Merge with DHIS2 core
    
    * Interim commit

--- R/utils.R
+++ R/utils.R
@@ -57,12 +57,11 @@ transformYAMLtoJSON <- function(filepath) {
 
 }
 
-transformYAMLtoControlFile<-function(include_protected = FALSE, include_slow = FALSE) {
+transformYAMLtoControlFile<-function(dir_path = getwd(), include_protected = FALSE, include_slow = FALSE) {
   all_yaml_files<-list.files("yaml/",pattern="*.yaml",recursive = TRUE,full.names = TRUE)
 
   d <- purrr::map_dfr(all_yaml_files, yaml::read_yaml) %>%
-    dplyr::mutate(is_protected = dplyr::case_when(is.na(is_protected) ~ FALSE,
-    TRUE ~ is_protected))  %>%
+    dplyr::mutate(is_protected = FALSE)  %>%
     dplyr::mutate(is_slow = dplyr::case_when(is.na(is_slow) ~ FALSE,
                                                   TRUE ~ is_slow))  %>%
     dplyr::arrange(section,section_order)
@@ -77,8 +76,8 @@ transformYAMLtoControlFile<-function(include_protected = FALSE, include_slow = F
       dplyr::select(name,description,summary_uid,details_uid) %>%
       dplyr::bind_cols(dups) %>%
       dplyr::filter(has_dup > 0)
-    print(duplicates)
-    stop("Duplicates found!")
+    warning("Duplicates found!")
+    return(duplicates)
   }
 
 #Filter any protected tables
@@ -127,6 +126,60 @@ transformYAMLtoMetadata <- function(include_protected=FALSE,include_slow = FALSE
 
 }
 
+transformYAMLtoExporter <- function(dir_path = getwd(),include_protected=TRUE,include_slow = FALSE) {
+  r <- transformYAMLtoControlFile(dir_path = dir_path,include_protected,include_slow)
+  #Prefix with dhis
+  r$name <- paste0('dhis_', r$name)
+  #Use ratio instead of percentage
+  r$summary_sql <- gsub("100?([\\.0 ]+)\\*","", r$summary_sql)
+  r$summary_sql <- gsub("as percent","as ratio", r$summary_sql)
+  appendWithSpaces <-function(string,path,indent){
+    if (indent > 0) {
+      indent_prefix <- paste(rep(" ",indent),sep="",collapse="")
+    } else {
+      indent_prefix <- ""
+    }
+
+
+    cat(paste0(indent_prefix,string),file=path,append = TRUE, sep="\n")
+  }
+
+  file_name <- "dhis_metadata_sql_exporter.yaml"
+  unlink(file_name)
+  cat("collector_name: dhis_system",file=file_name, sep="\n")
+  appendWithSpaces("metrics:", path=file_name,indent = 0)
+
+  for (i in seq_len(NROW(r))) {
+    appendWithSpaces(paste0("- metric_name: ",r$name[i]), path=file_name,indent = 2)
+    appendWithSpaces("type: gauge", path=file_name,indent = 4)
+    appendWithSpaces(paste0("help : ", paste0("'",r$description[i],"'")), path=file_name,indent = 4)
+    appendWithSpaces("values: [ratio]", path=file_name,indent = 4)
+    appendWithSpaces("query: |", path=file_name,indent = 4)
+    appendWithSpaces(r$summary_sql[i], path=file_name,indent = 6)
+  }
+
+}
+
+addcols <- function(data, cnames, type = "character") {
+  add <- cnames[!cnames %in% names(data)] # Subsets column name list BY only
+  # keeping names that are NOT in the supplied dataframes column names already.
+
+  if (length(add) != 0) { #If their are columns that need to be filled in THEN
+    #Impute the NA value based upon the type provided in the function.
+    # TODO: #Automate the character type or at least a list variable for type.
+    if (type == "character") {
+      data[add] <- NA_character_
+    } else if (type == "numeric") {
+      data[add] <- NA_real_
+    } else if (type == "logical") {
+      data[add] <- NA
+    }
+  }
+
+  return(data)
+
+}
+
 getSQLView <- function(uid, d2_session) {
 
   # #Execute the view first to be sure its fresh
@@ -138,9 +191,8 @@ getSQLView <- function(uid, d2_session) {
   # }
 
 
-  r <- tryCatch( httr::GET(paste0(d2_session$url,"api/sqlViews/",uid,"/data.json"),
+  r <- tryCatch( httr::GET(paste0(d2_session$url,"api/sqlViews/",uid,"/data.csv"),
                      handle=d2_session,
-                     content_type_json(),
                      timeout(600)) ,
                  error = function(e) print(e) )
 
@@ -149,29 +201,44 @@ getSQLView <- function(uid, d2_session) {
     return(NULL)
   }
 
-  if (r$status_code == 500L) {
+  if (r$status_code != 200L) {
     return( list(
       uid = uid,
       response_code = r$status_code,
-      message = httr::content(r)$message
+      message = ifelse(is.null(httr::content(r)$message),"Unknown error",httr::content(r)$message)
     ) )
   }
 
   if (r$status_code == 200L) {
-   resp <-  r %>%
-      httr::content("text") %>%
-      jsonlite::fromJSON(.)
 
-   data <- as.data.frame(resp$listGrid$rows)
-   #If there are no rows...this should never happen
-   if (NROW(data) == 0) {
-     return(NULL)
+    r %>%
+      httr::content("text") %>%
+      { # nolint
+        suppressWarnings(readr::read_csv(
+          .,
+          col_names = TRUE,
+          col_types = readr::cols(
+            .default = "c"
+          )
+        ))
+      }
+     }
+
+}
+
+getSummaryView <- function(uid,d2_session) {
+   x <- getSQLView(uid,d2_session)
+
+   if (inherits(x,"list")) {
+     return(x)
    }
 
-   names(data) <- resp$listGrid$headers$name
-   data$uid <- uid
-   return(data)
-  }
+   if (inherits(x, "data.frame")) {
+     x %>%
+       addcols(.,c("value","percent","count")) %>%
+       dplyr::mutate(uid = uid) %>%
+       dplyr::select(c("uid","value","percent","count"))
+   }
 
 }
 
